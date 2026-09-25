b20d7418f336a67ca8b018a49799f87dfd089429
keshavsoft <nknnkumar@yahoo.com>
2026-09-25 11:57:30 +0530

    src 6

--- Test/v1/vouchers/asIs/purchasesAll.js
+++ Test/v1/vouchers/asIs/purchasesAll.js
@@ -1,7 +1,7 @@
 import { vouchers } from "../../../../src/index.js";
-import { saveOutput } from "../../common/index.js";
+// import { saveOutput } from "../../common/index.js";
 
 const vouchersData = await vouchers.purchases.all("mani9");
 // saveOutput({ callerFile: import.meta.url, inData: vouchersData });
-console.log("vouchersData", vouchersData.length, vouchersData[0]);
+console.log("vouchersData", vouchersData.ENVELOPE.BODY.DATA.COLLECTION.VOUCHER.length);
 // console.log("vouchersData", vouchersData.ENVELOPE.BODY.DATA.COLLECTION.VOUCHER.length);
